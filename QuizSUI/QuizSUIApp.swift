dfb21ed47f210ceb4f547e1d93f970d648e29f1e
Paul <pasha.makey@gmail.com>
2024-05-05 18:13:32 +0300

    Initial Commit

---
+++ QuizSUI/QuizSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizSUIApp.swift
+//  QuizSUI
+//
+//  Created by Paul Makey on 5.05.24.
+//
+
+import SwiftUI
+
+@main
+struct QuizSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
